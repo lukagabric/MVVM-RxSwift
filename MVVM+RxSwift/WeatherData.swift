b6f6022bdbc22e54dff54f32ba820e7b3e1baeb3
Luka Gabric <luka.gabric@gmail.com>
2017-02-14 12:47:33 +0100

    Initial commit

---
+++ MVVM+RxSwift/WeatherData.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherData.swift
+//  MVVM+RxSwift
+//
+//  Created by Luka Gabric on 14/02/2017.
+//  Copyright © 2017 lukagabric.com. All rights reserved.
+//
+
+import Foundation
+
+public struct WeatherData {
+    
+    var locationName: String
+    var temperature: Float
+    var realFeel: Float
+    var precipitationPercentage: Float
+    var updatedAt: Date
+    
+}
---
+++ MVVM+RxSwift/WeatherDataService.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherDataService.swift
+//  MVVM+RxSwift
+//
+//  Created by Luka Gabric on 14/02/2017.
+//  Copyright © 2017 lukagabric.com. All rights reserved.
+//
+
+import Foundation
